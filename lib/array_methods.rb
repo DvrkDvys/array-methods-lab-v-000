37908d5b2e3578a1d0014605ddbac6e63f32b8a0
Jordan Harris <jharris@wework.com>
2018-06-22 16:50:32 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,7 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
@@ -25,31 +25,31 @@ end
 
 
 
-describe "#using_reverse" do 
-  it "takes in argument of an array and returns that same array with the contents reversed." do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_reverse(array).first).to eq("arrays!")
-    expect(using_reverse(array).last).to eq("wow")
-  end
-end
+# describe "#using_reverse" do 
+#   it "takes in argument of an array and returns that same array with the contents reversed." do 
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     expect(using_reverse(array).first).to eq("arrays!")
+#     expect(using_reverse(array).last).to eq("wow")
+#   end
+# end
 
-describe "#using_first" do 
-  it "takes in argument of an array and and returns the first element in the array" do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_first(array)).to eq("wow")
-  end
-end
+# describe "#using_first" do 
+#   it "takes in argument of an array and and returns the first element in the array" do 
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     expect(using_first(array)).to eq("wow")
+#   end
+# end
 
-describe "#using_last" do 
-  it "takes in argument of an array and and returns the last element in the array" do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_last(array)).to eq("arrays!")
-  end
-end
+# describe "#using_last" do 
+#   it "takes in argument of an array and and returns the last element in the array" do 
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     expect(using_last(array)).to eq("arrays!")
+#   end
+# end
 
-describe "#using_size" do 
-  it "takes in argument of an array and and returns the size, or length, of the array" do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_size(array)).to eq(6)
-  end
-end
+# describe "#using_size" do 
+#   it "takes in argument of an array and and returns the size, or length, of the array" do 
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     expect(using_size(array)).to eq(6)
+#   end
+# end
